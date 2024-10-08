dbb631e474f1a791292ea2bbb87b38d315f60671
JosephineJacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-10-08 22:03:10 +0200

    footer completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Josephine Jacobs
+        </a>{" "}
+        open-sourced on{" "}
+        <a
+          href="https://github.com/JosephineJacobs/weather-react-123"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and website hosted on{" "}
+        <a
+          href="https://weather-react-123.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
