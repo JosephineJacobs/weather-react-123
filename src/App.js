0cf9e3064d7f81792b301fd63dcc79759af0e507
JosephineJacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-10-09 19:30:26 +0200

    created weather js and css

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather";
+
+
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Josephine Jacobs
-        </a>{" "}
-        open-sourced on{" "}
-        <a
-          href="https://github.com/JosephineJacobs/weather-react-123"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and website hosted on{" "}
-        <a
-          href="https://weather-react-123.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
-    </div>
+        <h1>Weather App</h1>
+
+        <Weather />
+        <button className="btn btn-primary">Hello</button>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Josephine Jacobs
+          </a>{" "}
+          open-sourced on{" "}
+          <a
+            href="https://github.com/JosephineJacobs/weather-react-123"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and website hosted on{" "}
+          <a
+            href="https://weather-react-123.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
