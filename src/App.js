d7f74dbb2de9ed4f3d1392f76461f9120b9f3cc1
JosephineJacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-10-09 21:32:45 +0200

    got to redo..;-C

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+      
 
         <Weather />
-        <button className="btn btn-primary">Hello</button>
+      
 
         <footer>
           This project was coded by{" "}
